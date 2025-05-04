4563b1c7277aa95810d0cd172db1727ca7148e65
humanfriend22 <humanfriend22@gmail.com>
2025-05-04 10:52:07 -0700

    public api key is fine

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,12 +13,12 @@ export default defineNuxtConfig({
     runtimeConfig: {
         public: {
             firebase: {
-                apiKey: "",
+                apiKey: "AIzaSyBn9nC-Tuavh3Rn_7bVaI4SWT66Qu1KpTs",
                 authDomain: "gael-force-robotics-hf.firebaseapp.com",
                 projectId: "gael-force-robotics-hf",
                 storageBucket: "gael-force-robotics-hf.firebasestorage.app",
-                messagingSenderId: "",
-                appId: "",
+                messagingSenderId: "585677099356",
+                appId: "1:585677099356:web:f682381cb210a4abcff199",
             },
         },
     },
