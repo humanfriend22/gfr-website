4608470776177ab43607d611a4cbf5ad9324c6cc
humanfriend22 <humanfriend22@gmail.com>
2025-07-30 19:33:02 -0700

    remove duped team cards

--- app/pages/admin/teams.vue
+++ app/pages/admin/teams.vue
@@ -158,28 +158,7 @@ onMounted(async () => {
 
                 <div class="grid grid-cols-2 gap-4">
                     <TeamCard v-for="team of season.teams" :team="team" @click="showEditTeamModal(season, team)" />
-                    <div v-for="team of season.teams" class="flex gap-3 p-4 bg-base-200 hover:bg-base-300 cursor-pointer duration-300 rounded-lg" @click="showEditTeamModal(season, team)">
-                        <div>
-                            <TeamLogoDisplay :src="team.logo" :width="100" :height="100" />
-                            <!-- <NuxtImg src="RLogo.png" width="100" height="100" class="rounded-md select-none" /> -->
-                        </div>
-                        <div class="w-full text-gray-300">
-                            <div class="flex items-center justify-between text-white">
-                                <h1 class="font-bold text-lg">{{ team.name }}</h1>
-                                <button class="btn btn-ghost btn-square hover:text-gray-400 invisible">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                    </svg>
-                                </button>
-                            </div>
-                            <div>
-                                <h1 class="font-semibold">
-                                    <span>{{team.captains.map(uid => userFromUID(uid)?.name).join(' & ')}}</span>
-                                </h1>
-                            </div>
-                        </div>
-                    </div>
+
                 </div>
             </div>
         </ClientOnly>
